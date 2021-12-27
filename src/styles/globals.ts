9a4064b85b28302f47d57159ce93258491906d88
Joao Dias <joaoeduardodias123@gmail.com>
2021-12-27 08:15:40 -0300

    feat: add color in globalStyles

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -2,6 +2,18 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
 
+:root {
+    --background: #E5E5E5;
+    --bg-white: #FFFFFF;
+    --brandcolor-primary-default: #4F46BB;
+    --border-tag: ##8E85FF;
+    --textcolor-primary: #302E45;
+    --textcolor-secondary: #6D6C7B;
+    ---border-divider: #BBB8D9
+  }
+
+
+
 * {
   margin: 0;
   padding: 0;
@@ -21,6 +33,9 @@ export const GlobalStyle = createGlobalStyle`
 }
 body, input, textarea, select, button {
   font: 400  1rem  'Inter', sans-serif; 
+}
+body {
+  -webkit-font-smoothing: antialiased;
 }
 button {
   cursor: pointer;
