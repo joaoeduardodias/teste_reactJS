0144b74120991160569348a66d6dc841eda9efbb
Joao Dias <joaoeduardodias123@gmail.com>
2021-12-28 09:22:27 -0300

    feat: add component Tag

---
+++ src/components/Tag/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from './styles';
+
+interface TagProps {
+  text: string;
+}
+
+export function Tag({ text }: TagProps): JSX.Element {
+  return <Container type="button">{text}</Container>;
+}
---
+++ src/components/Tag/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+  height: 1.75rem;
+  width: 7.06rem;
+  padding: 0.5rem 1.125rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  color: var(--textcolor-primary);
+  font-size: 0.75rem;
+  border-radius: 71px;
+  border: 1px solid #8e85ff;
+  background: transparent;
+`;
